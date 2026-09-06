0d34f8c625a5df6c1f9143131b143381ca033992
Daniel Díaz <diaz_carrete@yahoo.com>
2026-09-06 11:21:22 +0200

    ...

--- lib/Compowrap.hs
+++ lib/Compowrap.hs
@@ -1,4 +1,13 @@
+{-# LANGUAGE RequiredTypeArguments #-}
+{-# LANGUAGE FunctionalDependencies #-}
 module Compowrap (someFunc) where
+import GHC.TypeLits (Natural)
+import Data.Functor.Compose
+import Data.Proxy
+
+
+class Compowrap (n :: Natural) outer inner inner_ tip | n inner -> inner_ tip where
+    askWrapUnwrap :: Proxy n -> outer inner  -> Compose outer inner_ tip
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
