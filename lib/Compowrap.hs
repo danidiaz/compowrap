a45dba6a62eed19f94c27080c21853e4a65cea77
Daniel Díaz <diaz_carrete@yahoo.com>
2026-09-20 17:11:20 +0200

    Improve type inference
    Remove the 'Compowrapped' type family and add a 'wrapped' type parameter
    to 'Compowrappable' that takes its place.
    This seems to improve inference. Some test bindings that required
    signatures now do not.

--- lib/Compowrap.hs
+++ lib/Compowrap.hs
@@ -6,7 +6,7 @@
 {-# LANGUAGE TypeAbstractions #-}
 {-# LANGUAGE TypeFamilies #-}
 
-module Compowrap (Compowrappable, Compowrapped, askWrapUnwrap, Compowrap (..)) where
+module Compowrap (Compowrappable, askWrapUnwrap, Compowrap (..)) where
 
 import Data.Coerce
 import Data.Functor.Compose
@@ -15,33 +15,35 @@ import Data.List
 import Data.Proxy
 import GHC.TypeLits (Natural)
 
-type Compowrapped :: List (Type -> Type) -> Type -> Type
-type family Compowrapped ts :: Type -> Type where
-  Compowrapped [f, g] = Compose f g
-  Compowrapped (f : rest) = Compose f (Compowrapped rest)
+type Compowrappable :: Natural -> List (Type -> Type) -> Type -> Type -> Type -> Constraint
+class
+  Compowrappable n ts tip unwrapped wrapped
+    | ts -> n,
+      n unwrapped -> ts tip,
+      n wrapped -> ts tip,
+      ts tip -> unwrapped,
+      ts tip -> wrapped
+  where
+  _wrap :: Proxy n -> unwrapped -> wrapped
+  _unwrap :: Proxy n -> wrapped -> unwrapped
 
-type Compowrappable :: Natural -> List (Type -> Type) -> Type -> Type -> Constraint
-class Compowrappable n ts tip whole | ts -> n, n whole -> ts tip, ts tip -> whole where
-  _wrap :: Proxy n -> whole -> Compowrapped ts tip
-  _unwrap :: Proxy n -> Compowrapped ts tip -> whole
-
-instance Compowrappable 2 [f, g] tip (f (g tip)) where
+instance Compowrappable 2 [f, g] tip (f (g tip)) (Compose f g tip) where
   _wrap Proxy = coerce
   _unwrap Proxy = coerce
 
-instance (Functor f) => Compowrappable 3 [f, g, h] tip (f (g (h tip))) where
+instance (Functor f) => Compowrappable 3 [f, g, h] tip (f (g (h tip))) (Compose f (Compose g h) tip) where
   _wrap Proxy = Compose . fmap Compose
   _unwrap Proxy = fmap getCompose . getCompose
 
-instance (Functor f, Functor g) => Compowrappable 4 [f, g, h, i] tip (f (g (h (i tip)))) where
+instance (Functor f, Functor g) => Compowrappable 4 [f, g, h, i] tip (f (g (h (i tip)))) ((Compose f (Compose g (Compose h i))) tip) where
   _wrap Proxy = Compose . fmap (Compose . fmap Compose)
   _unwrap Proxy = fmap (fmap getCompose . getCompose) . getCompose
 
 type Compowrap :: Natural -> List (Type -> Type) -> Type
 data Compowrap n ts
   = WrapUnwrap
-  { wrap :: forall tip whole. (Compowrappable n ts tip whole) => whole -> Compowrapped ts tip,
-    unwrap :: forall tip whole. (Compowrappable n ts tip whole) => Compowrapped ts tip -> whole
+  { wrap :: forall tip unwrapped wrapped. (Compowrappable n ts tip unwrapped wrapped) => unwrapped -> wrapped,
+    unwrap :: forall tip unwrapped wrapped. (Compowrappable n ts tip unwrapped wrapped) => wrapped -> unwrapped
   }
 
 askWrapUnwrap ::
--- test/Main.hs
+++ test/Main.hs
@@ -12,14 +12,34 @@ stuff' =
         & fmap not
         & unwrap
 
--- removing this signature causes a compilation error. But should it work without it?
-stuff'' :: Either Int (Either Int (Either Int String))
+-- works even with signature.
+-- I had to add the 'wrapped' type variable to 'Compowrappable' for this to work.
+-- stuff'' :: Either Int (Either Int (Either Int String))
 stuff'' =
   let WrapUnwrap {wrap, unwrap} = askWrapUnwrap 3
    in unwrap $ show <$> ((&&) <$> wrap stuff <*> wrap stuff)
 
+atuff, atuff' :: Either String (Either Float (Either Int Bool))
+atuff = Right (Right (Right False))
+atuff' =
+  let WrapUnwrap {wrap, unwrap} = askWrapUnwrap 3
+   in atuff
+        & wrap
+        & fmap not
+        & unwrap
+
+-- works even with signature.
+-- I had to add the 'wrapped' type variable to 'Compowrappable' for this to work.
+-- astuff'' :: Either Int (Either Int (Either Int String))
+atuff'' =
+  let WrapUnwrap {wrap, unwrap} = askWrapUnwrap 3
+   in unwrap $ show <$> ((&&) <$> wrap atuff <*> wrap atuff)
+
 main :: IO ()
 main = do
-    putStrLn $ show stuff
-    putStrLn $ show stuff'
-    putStrLn $ show stuff''
+  putStrLn $ show stuff
+  putStrLn $ show stuff'
+  putStrLn $ show stuff''
+  putStrLn $ show atuff
+  putStrLn $ show atuff'
+  putStrLn $ show atuff''
