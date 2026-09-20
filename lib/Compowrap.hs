f46e7476c0046b605405f96b4fe7f0974b001810
Daniel Díaz <diaz_carrete@yahoo.com>
2026-09-20 16:19:34 +0200

    ...

--- lib/Compowrap.hs
+++ lib/Compowrap.hs
@@ -1,62 +1,56 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE RequiredTypeArguments #-}
-{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeAbstractions #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeFamilies #-}
 
-module Compowrap (Compowrappable, Compowrapped, askWrapUnwrap, Compowrap(..)) where
+module Compowrap (Compowrappable, Compowrapped, askWrapUnwrap, Compowrap (..)) where
 
+import Data.Coerce
 import Data.Functor.Compose
+import Data.Kind
+import Data.List
 import Data.Proxy
 import GHC.TypeLits (Natural)
-import Data.List
-import Data.Kind
-import Data.Coerce
 
 type Compowrapped :: List (Type -> Type) -> Type -> Type
 type family Compowrapped ts :: Type -> Type where
-  Compowrapped [f, g] = Compose f g 
+  Compowrapped [f, g] = Compose f g
   Compowrapped (f : rest) = Compose f (Compowrapped rest)
 
 type Compowrappable :: Natural -> List (Type -> Type) -> Type -> Type -> Constraint
 class Compowrappable n ts tip whole | ts -> n, n whole -> ts tip, ts tip -> whole where
-   
-  askWrapUnwrap_ 
-    :: 
-      Proxy n ->
-      (whole -> Compowrapped ts tip,
-      Compowrapped ts tip -> whole)
+  _wrap :: Proxy n -> whole -> Compowrapped ts tip
+  _unwrap :: Proxy n -> Compowrapped ts tip -> whole
 
-instance Compowrappable 2 [f,g] tip (f (g tip)) where
-  askWrapUnwrap_ Proxy = (\u -> coerce u , \w -> coerce w)
+instance Compowrappable 2 [f, g] tip (f (g tip)) where
+  _wrap Proxy = coerce
+  _unwrap Proxy = coerce
 
-instance Functor f => Compowrappable 3 [f,g,h] tip (f (g (h tip))) where
-  askWrapUnwrap_ Proxy = (Compose . fmap Compose, fmap getCompose . getCompose)
+instance (Functor f) => Compowrappable 3 [f, g, h] tip (f (g (h tip))) where
+  _wrap Proxy = Compose . fmap Compose
+  _unwrap Proxy = fmap getCompose . getCompose
 
-instance (Functor f, Functor g) => Compowrappable 4 [f,g,h,i] tip (f (g (h (i tip)))) where
-  askWrapUnwrap_ Proxy = (Compose . fmap (Compose . fmap Compose), fmap (fmap getCompose . getCompose) . getCompose)
+instance (Functor f, Functor g) => Compowrappable 4 [f, g, h, i] tip (f (g (h (i tip)))) where
+  _wrap Proxy = Compose . fmap (Compose . fmap Compose)
+  _unwrap Proxy = fmap (fmap getCompose . getCompose) . getCompose
 
 type Compowrap :: Natural -> List (Type -> Type) -> Type
 data Compowrap n ts where
-  WrapUnwrap :: 
-    { wrap :: forall tip whole . Compowrappable n ts tip whole => whole -> Compowrapped ts tip,
-      unwrap :: forall tip whole . Compowrappable n ts tip whole => Compowrapped ts tip -> whole
-     } -> Compowrap n ts
+  WrapUnwrap ::
+    { wrap :: forall tip whole. (Compowrappable n ts tip whole) => whole -> Compowrapped ts tip,
+      unwrap :: forall tip whole. (Compowrappable n ts tip whole) => Compowrapped ts tip -> whole
+    } ->
+    Compowrap n ts
 
-askWrapUnwrap 
-  :: 
-     forall {ts}. 
-     forall n -> 
-     Compowrap n ts
-askWrapUnwrap tn = 
+askWrapUnwrap ::
+  forall {ts}.
+  forall n ->
+  Compowrap n ts
+askWrapUnwrap tn =
   WrapUnwrap
-    { wrap = 
-        let (wrap, _) = askWrapUnwrap_ @_ @ts (Proxy @tn)
-         in wrap
-      ,
-      unwrap =
-        let (_, unwrap) = askWrapUnwrap_ @_ @ts (Proxy @tn)
-         in unwrap
-     }
+    { wrap = _wrap @_ @ts (Proxy @tn),
+      unwrap = _unwrap @_ @ts (Proxy @tn)
+    }
--- test/Main.hs
+++ test/Main.hs
@@ -3,15 +3,19 @@ module Main (main) where
 import Compowrap
 import Data.Function ((&))
 
-stuff, stuff' :: Either Int (Either Int (Either Int Bool))
+stuff, stuff', stuff'' :: Either Int (Either Int (Either Int Bool))
 stuff = Right (Right (Right False))
 
 stuff' =  
-    let (wrap, unwrap) = askWrapUnwrap 3
+    let WrapUnwrap {wrap, unwrap} = askWrapUnwrap 3
      in stuff
         & wrap
         & fmap not 
         & unwrap
+
+stuff'' =  
+    let WrapUnwrap {wrap, unwrap} = askWrapUnwrap 3
+     in unwrap $ and <$> wrap stuff <*> wrap stuff
 
 main :: IO ()
 main = putStrLn "Test suite not yet implemented."
