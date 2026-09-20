53aa860ce86f6485b4ba168af6416d903a78a442
Daniel Díaz <diaz_carrete@yahoo.com>
2026-09-20 19:18:28 +0200

    Remove tip type parameter.

--- lib/Compowrap.hs
+++ lib/Compowrap.hs
@@ -12,25 +12,24 @@ import Data.Kind
 import Data.Proxy
 import GHC.TypeLits (Natural)
 
-type Compowrappable :: Natural -> Type -> Type -> Type -> Constraint
+type Compowrappable :: Natural -> Type -> Type -> Constraint
 class
-  Compowrappable n tip unwrapped wrapped
-    | 
-      n unwrapped -> wrapped tip,
-      n wrapped -> unwrapped tip
+  Compowrappable n unwrapped wrapped
+    | n unwrapped -> wrapped,
+      n wrapped -> unwrapped
   where
   _wrap :: Proxy n -> unwrapped -> wrapped
   _unwrap :: Proxy n -> wrapped -> unwrapped
 
-instance Compowrappable 2 tip (f (g tip)) (Compose f g tip) where
+instance Compowrappable 2 (f (g tip)) (Compose f g tip) where
   _wrap Proxy = coerce
   _unwrap Proxy = coerce
 
-instance (Functor f) => Compowrappable 3 tip (f (g (h tip))) (Compose f (Compose g h) tip) where
+instance (Functor f) => Compowrappable 3 (f (g (h tip))) (Compose f (Compose g h) tip) where
   _wrap Proxy = Compose . fmap Compose
   _unwrap Proxy = fmap getCompose . getCompose
 
-instance (Functor f, Functor g) => Compowrappable 4 tip (f (g (h (i tip)))) ((Compose f (Compose g (Compose h i))) tip) where
+instance (Functor f, Functor g) => Compowrappable 4 (f (g (h (i tip)))) ((Compose f (Compose g (Compose h i))) tip) where
   _wrap Proxy = Compose . fmap (Compose . fmap Compose)
   _unwrap Proxy = fmap (fmap getCompose . getCompose) . getCompose
 
@@ -39,8 +38,8 @@ instance (Functor f, Functor g) => Compowrappable 4 tip (f (g (h (i tip)))) ((Co
 type Compowrap :: Natural -> Type
 data Compowrap n
   = WrapUnwrap
-  { wrap :: forall tip {unwrapped} {wrapped}. (Compowrappable n tip unwrapped wrapped) => unwrapped -> wrapped,
-    unwrap :: forall tip {unwrapped} {wrapped}. (Compowrappable n tip unwrapped wrapped) => wrapped -> unwrapped
+  { wrap :: forall {unwrapped} {wrapped}. (Compowrappable n unwrapped wrapped) => unwrapped -> wrapped,
+    unwrap :: forall {unwrapped} {wrapped}. (Compowrappable n unwrapped wrapped) => wrapped -> unwrapped
   }
 
 askWrapUnwrap ::
