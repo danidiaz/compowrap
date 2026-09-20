192ce132279985a5a2e18da485422c9b65af8e63
Daniel Díaz <diaz_carrete@yahoo.com>
2026-09-20 16:26:54 +0200

    ...

--- test/Main.hs
+++ test/Main.hs
@@ -3,19 +3,19 @@ module Main (main) where
 import Compowrap
 import Data.Function ((&))
 
-stuff, stuff', stuff'' :: Either Int (Either Int (Either Int Bool))
+stuff, stuff' :: Either Int (Either Int (Either Int Bool))
 stuff = Right (Right (Right False))
-
-stuff' =  
-    let WrapUnwrap {wrap, unwrap} = askWrapUnwrap 3
-     in stuff
+stuff' =
+  let WrapUnwrap {wrap, unwrap} = askWrapUnwrap 3
+   in stuff
         & wrap
-        & fmap not 
+        & fmap not
         & unwrap
 
-stuff'' =  
-    let WrapUnwrap {wrap, unwrap} = askWrapUnwrap 3
-     in unwrap $ and <$> wrap stuff <*> wrap stuff
+stuff'' :: Either Int (Either Int (Either Int String))
+stuff'' =
+  let WrapUnwrap {wrap, unwrap} = askWrapUnwrap 3
+   in unwrap $ show <$> ((&&) <$> wrap stuff <*> wrap stuff)
 
 main :: IO ()
 main = putStrLn "Test suite not yet implemented."
